d84c1bee2b252f4687cc44bd0e84759d30602278
julia-francais <ju.francais@gmail.com>
2020-04-27 20:16:19 +0200

    add fetch countries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,12 @@
-import React, { useRef } from "react";
+import React from "react";
 import Helmet from "react-helmet";
 import L from "leaflet";
-import { Marker } from "react-leaflet";
 
-import { promiseToFlyTo, getCurrentLocation } from "lib/map";
+import axios from "axios";
 
 import Layout from "components/Layout";
 import Container from "components/Container";
 import Map from "components/Map";
-
-import gatsby_astronaut from "assets/images/gatsby-astronaut.jpg";
 
 const LOCATION = {
   lat: 38.9072,
@@ -17,35 +14,48 @@ const LOCATION = {
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
-const ZOOM = 10;
-
-const timeToZoom = 2000;
-const timeToOpenPopupAfterZoom = 4000;
-const timeToUpdatePopupAfterZoom = timeToOpenPopupAfterZoom + 3000;
-
-const popupContentHello = `<p>Hello 👋</p>`;
-const popupContentGatsby = `
-  <div class="popup-gatsby">
-    <div class="popup-gatsby-image">
-      <img class="gatsby-astronaut" src=${gatsby_astronaut} />
-    </div>
-    <div class="popup-gatsby-content">
-      <h1>Gatsby Leaflet Starter</h1>
-      <p>Welcome to your new Gatsby site. Now go build something great!</p>
-    </div>
-  </div>
-`;
 
 const IndexPage = () => {
-  const markerRef = useRef();
-
   /**
    * mapEffect
    * @description Fires a callback once the page renders
    * @example Here this is and example of being used to zoom in and set a popup on load
    */
 
-  async function mapEffect({ leafletElement: map } = {}) {}
+  async function mapEffect({ leafletElement: map } = {}) {
+    let response;
+
+    try {
+      response = await axios.get("https://corona.lmao.ninja/v2/countries");
+    } catch (error) {
+      console.log(`Failed to fetch the countries: ${error.message}`, error);
+      return;
+    }
+    const { data = [] } = response;
+    console.log(data);
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    if (!hasData) return;
+
+    const geoJson = {
+      type: "FeatureCollection",
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: "Feature",
+          properties: {
+            ...country,
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [lng, lat],
+          },
+        };
+      }),
+    };
+    console.log("geoJson", geoJson);
+  }
 
   const mapSettings = {
     center: CENTER,
